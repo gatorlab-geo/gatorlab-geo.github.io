6086eefba77836bce7d5cfb89d048c3b5b6dc017
jlevente <jlevente89@gmail.com>
2026-06-08 09:37:44 -0400

    Add szeged_2026 presentation

---
+++ presentations/szeged_2026/build.py
@@ -0,0 +1,13 @@
+import os
+
+parts = ['head.html', 'part1.html', 'part2.html', 'part3.html', 'part4.html', 'part5.html', 'tail.html']
+output_file = 'index.html'
+
+with open(output_file, 'w', encoding='utf-8') as outfile:
+    for part in parts:
+        part_path = os.path.join(os.path.dirname(__file__), part)
+        with open(part_path, 'r', encoding='utf-8') as infile:
+            outfile.write(infile.read())
+            outfile.write('\n')
+
+print(f"Successfully built {output_file}")
